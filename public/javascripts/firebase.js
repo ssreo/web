4a5b1fbdb6b258cdefe4c54fcd1d2c3ac36e3807
ssreo <ssreo3637@gmail.com>
2023-10-07 18:44:06 +0900

    seongwon1007

---
+++ public/javascripts/firebase.js
@@ -0,0 +1,21 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "https://www.gstatic.com/firebasejs/10.4.0/firebase-app.js";
+import { getAnalytics } from "https://www.gstatic.com/firebasejs/10.4.0/firebase-analytics.js";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+const firebaseConfig = {
+  apiKey: "AIzaSyC8ux9AzT3yeOq57klfdKlmNXL1IX95Xr0",
+  authDomain: "fir-b6057.firebaseapp.com",
+  projectId: "fir-b6057",
+  storageBucket: "fir-b6057.appspot.com",
+  messagingSenderId: "770096561054",
+  appId: "1:770096561054:web:7dfb647089edfda985f48f",
+  measurementId: "G-9W7KLV2WWM"
+};
+
+// Initialize Firebase
+export const app = initializeApp(firebaseConfig);
+const analytics = getAnalytics(app);
--- routes/users.js
+++ routes/users.js
@@ -6,4 +6,14 @@ router.get('/', function(req, res, next) {
   res.send('respond with a resource');
 });
 
+//로그인 페이지 이동
+router.get('/login', function(req, res){
+  res.render('index',{title:'로그인',pageName:'users/login.ejs'});
+});
+
+//회원가입 페이지 이동
+router.get('/join', function(req, res){
+  res.render('index',{title:'회원가입',pageName:'users/join.ejs'});
+});
+
 module.exports = router;

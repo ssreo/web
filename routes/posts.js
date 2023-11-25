b23c55d970564ef6e8c33383f70c8f4fa18fc38c
ssreo <ssreo3637@gmail.com>
2023-11-25 18:15:12 +0900

    1125

--- routes/posts.js
+++ routes/posts.js
@@ -2,12 +2,21 @@ var express = require('express');
 var router = express.Router();
 
 
-router.get('/', function(req, res){
+router.get('/', function(req, res,next){
     res.render('index', {title:'게시판',pageName:'posts/list.ejs'})
 });
 
-router.get('/insert', function(req, res){
+router.get('/insert', function(req, res,next){
     res.render('index', {title:'글쓰기',pageName:'posts/insert.ejs'})
+});
+
+router.get('/read', function(req, res,next){
+    const id=req.query.id;
+    res.render('index', {title:'게시글 정보',pageName:'posts/read.ejs', id})
+});
+router.get('/update', function(req, res,next){
+    const id=req.query.id;
+    res.render('index', {title:'게시글 수정',pageName:'posts/update.ejs', id})
 });
 
 module.exports = router;
